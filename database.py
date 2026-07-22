ca0dd679d9adb85300d72e98eecc925171a22db0
masa607 <masa.06.07@gmail.com>
2026-07-22 14:10:18 +0900

    add:envファイルとdatabase.pyの作成をしました。

---
+++ database.py
@@ -0,0 +1,34 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+# .env ファイルから環境変数を読み込む
+load_dotenv()
+
+# .env から DB 接続情報を取得
+# 例: mysql+pymysql://ユーザー名:パスワード@ホスト名:ポート/データベース名
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
+
+# SQLAlchemy エンジンの作成
+engine = create_engine(
+    DATABASE_URL,
+    echo=True  # 実行されるSQLをターミナルに表示（デバッグ用）
+)
+
+# データベースセッションの作成
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# モデルの親クラス
+Base = declarative_base()
+
+# DBセッションを取得・終了するための依存関係関数
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
